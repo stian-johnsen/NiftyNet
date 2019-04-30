fa4c7e90b5532b93ddcded38bbc15160e547198a
Stian Johnsen <stian.johnsen@kcl.ac.uk>
2019-04-30 16:58:15 +0100

    Split image_sink.py into a base and an F/S module

---
+++ niftynet/io/image_sink_base.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Image output module
+"""
+from __future__ import absolute_import
+
+from abc import ABCMeta, abstractmethod
+
+from niftynet.layer.base_layer import Layer
+
+class ImageSinkBase(Layer):
+    """
+    Base class for passthrough layers that write images.
+    """
+
+    def __init__(self,
+                 source,
+                 interp_order,
+                 name='image_sink'):
+        """
+        :param source: the image source of the input images
+        for which this layer is to write the outputs.
+        :param interp_order: polynomial order of the interpolation applied
+        where needed on output.
+        """
+
+        super(ImageSinkBase, self).__init__(name=name)
+
+        self._source = source
+        self.interp_order = interp_order
+
+    @property
+    def source(self):
+        """
+        :return: the source of the images written by this layer
+        """
+
+        return self._source
+
+    @abstractmethod
+    def layer_op(self, image_data_out, image_id, image_data_in):
+        """
+        :param image_data_out: the voxel data to output
+        :param image_id: the ID associated with the data
+        :param image_data_in: the image object from which the output
+        was generated
+        """
+
+        return
--- niftynet/io/image_writer.py
+++ niftynet/io/image_writer.py
@@ -1,54 +1,13 @@
+# -*- coding: utf-8 -*-
 """
-Image output module
+Image F/S output module
 """
 from __future__ import absolute_import
 
-from abc import ABCMeta, abstractmethod
 import os.path
 
+from niftynet.io.image_sink_base import ImageSinkBase
 import niftynet.io.misc_io as misc_io
-from niftynet.layer.base_layer import Layer
-
-class ImageSinkBase(Layer):
-    """
-    Base class for passthrough layers that write images.
-    """
-
-    def __init__(self,
-                 source,
-                 interp_order,
-                 name='image_sink'):
-        """
-        :param source: the image source of the input images
-        for which this layer is to write the outputs.
-        :param interp_order: polynomial order of the interpolation applied
-        where needed on output.
-        """
-
-        super(ImageSinkBase, self).__init__(name=name)
-
-        self._source = source
-        self.interp_order = self.interp_order
-
-    @property
-    def source(self):
-        """
-        :return: the source of the images written by this layer
-        """
-
-        return self._source
-
-    @abstractmethod
-    def layer_op(self, image_data_out, image_id, image_data_in):
-        """
-        :param image_data_out: the voxel data to output
-        :param image_id: the ID associated with the data
-        :param image_data_in: the image object from which the output
-        was generated
-        """
-
-        return
-
 
 class ImageWriter(ImageSinkBase):
     """
@@ -69,6 +28,7 @@ class ImageWriter(ImageSinkBase):
 
         self.output_path = os.path.abspath(output_path)
         self.postfix = postfix
+        self.inferred_cleared = False
 
     def layer_op(self, image_data_out, image_id, image_data_in):
         subject_name = self.source.get_subject_id(image_id)
