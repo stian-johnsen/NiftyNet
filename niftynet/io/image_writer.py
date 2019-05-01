de7404de4bba79419d0521858efc718c1faadc5c
Stian Johnsen <stian.johnsen@kcl.ac.uk>
2019-05-01 14:26:51 +0100

    Added a ImageWriterBase interface to tag image sinks with F/S output

--- niftynet/io/image_writer.py
+++ niftynet/io/image_writer.py
@@ -4,12 +4,42 @@ Image F/S output module
 """
 from __future__ import absolute_import
 
+from abc import ABCMeta, abstractproperty
 import os.path
 
 from niftynet.io.image_sink_base import ImageSinkBase
 import niftynet.io.misc_io as misc_io
 
-class ImageWriter(ImageSinkBase):
+class ImageWriterBase(ImageSinkBase):
+    """
+    Interface for image sinks with F/S output
+    """
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self,
+                 reader,
+                 name='image_writer_base'):
+        super(ImageWriterBase, self).__init__(reader, name=name)
+
+    @abstractproperty
+    def output_path(self):
+        """
+        Output directory path
+        """
+
+        return
+
+    @abstractproperty
+    def postfix(self):
+        """
+        Filename stem suffix applied to output files.
+        """
+
+        return
+
+
+class ImageWriter(ImageWriterBase):
     """
     F/S output image writer class
     """
@@ -26,11 +56,21 @@ class ImageWriter(ImageSinkBase):
 
         super(ImageWriter, self).__init__(source, interp_order)
 
-        self.output_path = os.path.abspath(output_path)
-        self.postfix = postfix
+        self._output_path = os.path.abspath(output_path)
+        self._postfix = postfix
         self.inferred_cleared = False
 
+    @property
+    def output_path(self):
+        return self._output_path
+
+    @property
+    def postfix(self):
+        return self._postfix
+
     def layer_op(self, image_data_out, image_id, image_data_in):
+        image_data_out = self._invert_preprocessing(image_data_out)
+
         subject_name = self.source.get_subject_id(image_id)
         filename = "{}{}.nii.gz".format(subject_name, self.postfix)
         misc_io.save_data_array(self.output_path,
